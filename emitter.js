a5e9df11d570b2c791fc1ddf94c6f984104f186e
Admirallll <stanislav.yarkeev@gmail.com>
2017-11-01 01:09:13 +0500

    jobs done

--- emitter.js
+++ emitter.js
@@ -12,6 +12,9 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    let allEvents = {};
+    let namespaces = {};
+
     return {
 
         /**
@@ -19,26 +22,65 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Object} state
+         * @returns {Object} this emitter with new event
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, state) {
+            if (state === undefined) {
+                state = {
+                    currentState: 0,
+                    stateChecker: () => true
+                };
+            }
+            if (!(event in allEvents)) {
+                allEvents[event] = [];
+            }
+            let namespaceParts = event.split('.');
+            let currentNamespace = namespaces;
+            for (let namespacePart of namespaceParts) {
+                if (!(namespacePart in currentNamespace)) {
+                    currentNamespace[namespacePart] = {};
+                }
+                currentNamespace = currentNamespace[namespacePart];
+            }
+            allEvents[event].push({ context,
+                function: function () {
+                    handler.call(context);
+                },
+                state }
+            );
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} emitter
          */
         off: function (event, context) {
-            console.info(event, context);
+            let eventNames = getNamespacesForDelete(namespaces, event);
+            for (let eventName of eventNames) {
+                allEvents[eventName] = allEvents[eventName].filter(
+                    currentEvent => currentEvent.context !== context
+                );
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} emitter
          */
         emit: function (event) {
-            console.info(event);
+            for (let eventName of parseNamespace(event)) {
+                callEvent(allEvents, eventName);
+            }
+
+            return this;
         },
 
         /**
@@ -48,9 +90,16 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} emitter
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            let state = {
+                currentState: 0,
+                stateChecker: currentState => currentState <= times || times <= 0
+            };
+            this.on(event, context, handler, state);
+
+            return this;
         },
 
         /**
@@ -60,9 +109,72 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} emitter
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            let state = {
+                currentState: 1,
+                stateChecker: currentState => currentState % frequency === 0 || frequency <= 0
+            };
+            this.on(event, context, handler, state);
+
+            return this;
         }
     };
+}
+
+function callEvent(allEvents, eventName) {
+    if (!(eventName in allEvents)) {
+        return;
+    }
+    for (let event of allEvents[eventName]) {
+        event.state.currentState++;
+        if (!(event.state.stateChecker(event.state.currentState))) {
+            continue;
+        }
+        event.function();
+    }
+}
+
+function getNamespacesForDelete(namespaces, name) {
+    let namespaceParts = name.split('.');
+    for (let part of namespaceParts) {
+        if (!(part in namespaces)) {
+            return [];
+        }
+        namespaces = namespaces[part];
+    }
+    let result = getAllNamespacesRecursively(namespaces, name);
+    result.push(name);
+
+    return result;
+}
+
+function getAllNamespacesRecursively(namespaces, startNamespace) {
+    let result = [];
+    let currentNamespace = startNamespace;
+    for (let namespace of Object.keys(namespaces)) {
+        if (currentNamespace.length > 0) {
+            currentNamespace += '.';
+        }
+        result.push(currentNamespace + namespace);
+        currentNamespace += namespace;
+        for (let resultNamespace of getAllNamespacesRecursively(namespaces[namespace])) {
+            result.push(currentNamespace + '.' + resultNamespace);
+        }
+    }
+
+    return result;
+}
+
+function parseNamespace(namespace) {
+    let eventNames = [];
+    let dotIndex = -1;
+    do {
+        dotIndex = namespace.lastIndexOf('.');
+        eventNames.push(namespace);
+        namespace = namespace.substring(0, dotIndex);
+    } while (dotIndex > 0);
+
+    return eventNames;
 }
